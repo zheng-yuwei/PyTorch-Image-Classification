310c85584a5b9ffac56f3dfc5ec35a5f79183b87
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 02:54:25 +0800

    Update __init__.py

--- applications/__init__.py
+++ applications/__init__.py
@@ -3,3 +3,9 @@
 File __init__.py.py
 @author: ZhengYuwei
 """
+from .convert import convert_to_jit
+from .distillation import distill
+from .test import test
+from .train import train
+from .visualize import Visualize
+from .make_curriculum import make_curriculum
