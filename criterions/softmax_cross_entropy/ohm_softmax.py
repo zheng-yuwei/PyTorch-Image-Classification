f1499947be6669a7c6424c385f5b1811d609a7c4
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:54:59 +0800

    Update ohm_softmax.py

--- criterions/softmax_cross_entropy/ohm_softmax.py
+++ criterions/softmax_cross_entropy/ohm_softmax.py
@@ -2,4 +2,26 @@
 """
 File ohm_softmax.py
 @author: ZhengYuwei
+加入OHM的softmax交叉熵损失函数，分类index label和one hot label形式都支持 
 """
+import torch
+
+from .basic_softmax import CrossEntropyLoss
+
+
+class OHMCELoss(CrossEntropyLoss):
+    """ 困难样本学习 """
+    def __init__(self, args):
+        super(OHMCELoss, self).__init__()
+        self.hard_ratio = args.hard_ratio
+
+    def get_weights(self, predictions: torch.FloatTensor, targets: torch.FloatTensor):
+        """ 多分类交叉熵损失
+        :param predictions: 预测的概率矩阵，(batch_size, label_num)
+        :param targets: 解码后的多分类label概率矩阵，(batch_size, label_num)
+        """
+        gradients, _ = torch.abs(predictions - targets).max(dim=1)
+        ohm_positions = gradients.sort()[1][:int(gradients.numel() * self.hard_ratio)]
+        weights = torch.zeros_like(predictions)
+        weights[ohm_positions] = 1
+        return weights
