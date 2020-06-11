9e47b920ee6b8b5dacbfd152fb5fb0023eeb0e1a
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:59:42 +0800

    Update __init__.py

--- utils/cam_tool/__init__.py
+++ utils/cam_tool/__init__.py
@@ -2,4 +2,9 @@
 """
 File __init__.py.py
 @author: ZhengYuwei
+可视化工具：CAM、Grad-CAM、Grad-CAM++系列 
 """
+from .heatmap import HeatMapTool
+from .cam import CAM
+from .grad_cam import GradCAM
+from .grad_cam_plus import GradCamPlusPlus
