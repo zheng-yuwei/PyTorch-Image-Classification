adbf71dafc97fdf98eac9b1858c4c4982f1dbf4a
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:22:11 +0800

    Update threshold_bce.py

--- criterions/binary_cross_entropy/threshold_bce.py
+++ criterions/binary_cross_entropy/threshold_bce.py
@@ -2,4 +2,32 @@
 """
 File threshold_bce.py
 @author: ZhengYuwei
+带阈值控制的多标签二分类交叉熵损失 ThresholdCELoss
+非目标类需要大于指定阈值才计算loss，目标类需要小于指定阈值才计算loss，
+与制作软标签、hard-example mining有类似思想
 """
+import torch
+
+from .basic_bce import MultiLabelBCELoss
+from config import CriterionConstant
+
+
+class ThresholdBCELoss(MultiLabelBCELoss):
+    """ 借用软标签和困难样本的思想 """
+    def __init__(self, args):
+        super(ThresholdBCELoss, self).__init__()
+        self.low_threshold = torch.from_numpy(CriterionConstant.low_threshold_for_bce)
+        self.up_threshold = torch.from_numpy(CriterionConstant.up_threshold_for_bce)
+        if args.cuda:
+            self.low_threshold = self.low_threshold.cuda(args.gpu)
+            self.up_threshold = self.up_threshold.cuda(args.gpu)
+
+    def get_weights(self, predictions: torch.FloatTensor, targets: torch.FloatTensor):
+        """ 多标签二分类交叉熵损失
+        :param predictions: 预测的概率矩阵，(batch_size, label_num)
+        :param targets: 解码后的多标签二分类label概率矩阵，(batch_size, label_num)
+        """
+        low_threshold = targets @ self.low_threshold
+        up_threshold = targets @ self.up_threshold
+        weights = ((predictions > low_threshold) & (predictions < up_threshold)).float()
+        return weights
