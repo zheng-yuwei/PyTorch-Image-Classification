2a14a2acf089f9374ae9b1df3c7ad88790f7f9ed
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:21:14 +0800

    Update hybrid_bce.py

--- criterions/binary_cross_entropy/hybrid_bce.py
+++ criterions/binary_cross_entropy/hybrid_bce.py
@@ -2,4 +2,43 @@
 """
 File hybrid_bce.py
 @author: ZhengYuwei
+混合策略的多标签二分类交叉熵损失 
 """
+import torch
+
+from .basic_bce import MultiLabelBCELoss
+from .threshold_bce import ThresholdBCELoss
+from .weighted_bce import WeightedBCELoss
+from .ohm_bce import OHMBCELoss
+from .ghm_bce import GHMBCELoss
+
+
+class HybridBCELoss(MultiLabelBCELoss):
+    """ 自定义的多标签二分类损失函数，包含weighted loss，threshold loss，ghm loss,ohm loss等功能 """
+
+    def __init__(self, args):
+        super(HybridBCELoss, self).__init__()
+        self.threshold_func = self.identity_weights
+        self.weighted_func = self.identity_weights
+        self.ohm_func = self.identity_weights
+        self.ghm_func = self.identity_weights
+
+        if args.threshold_loss:
+            self.threshold_func = ThresholdBCELoss(args).get_weights
+        if args.weighted_loss:
+            self.weighted_func = WeightedBCELoss(args).get_weights
+        if args.ohm_loss:
+            self.ohm_func = OHMBCELoss(args).get_weights
+        if args.ghm_loss:
+            self.ghm_func = GHMBCELoss(args).get_weights
+
+    def get_weights(self, predictions: torch.FloatTensor, targets: torch.FloatTensor):
+        """ 多标签二分类交叉熵损失
+        :param predictions: 预测的概率矩阵，(batch_size, label_num)
+        :param targets: 解码后的多标签二分类label概率矩阵，(batch_size, label_num)
+        """
+        weights = self.threshold_func(predictions, targets)
+        weights *= self.weighted_func(predictions, targets)
+        weights *= self.ohm_func(predictions, targets)
+        weights *= self.ghm_func(predictions, targets)
+        return weights
