db9d8d1462c86af78963dc3006754880ec6d9f1e
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:52:22 +0800

    Update __init__.py

--- criterions/softmax_cross_entropy/__init__.py
+++ criterions/softmax_cross_entropy/__init__.py
@@ -2,4 +2,11 @@
 """
 File __init__.py.py
 @author: ZhengYuwei
+softmax交叉熵损失函数及其变体 
 """
+from .basic_softmax import CrossEntropyLoss
+from .ghm_softmax import GHMCELoss
+from .hybrid_softmax import HybridCELoss
+from .ohm_softmax import OHMCELoss
+from .threshold_softmax import ThresholdCELoss
+from .weighted_softmax import WeightedCELoss
