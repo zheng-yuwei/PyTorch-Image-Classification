da89b8d5ef9f5fd171a7f640a3d538ad4ad04bcf
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 16:56:20 +0800

    Update __init__.py

--- criterions/__init__.py
+++ criterions/__init__.py
@@ -2,4 +2,21 @@
 """
 File __init__.py.py
 @author: ZhengYuwei
+损失函数 
 """
+from .softmax_cross_entropy import (
+    CrossEntropyLoss,
+    ThresholdCELoss,
+    WeightedCELoss,
+    GHMCELoss,
+    OHMCELoss,
+    HybridCELoss
+)
+from .binary_cross_entropy import (
+    MultiLabelBCELoss,
+    ThresholdBCELoss,
+    WeightedBCELoss,
+    GHMBCELoss,
+    OHMBCELoss,
+    HybridBCELoss,
+)
