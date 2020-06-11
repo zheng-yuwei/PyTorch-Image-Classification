7c08e243eae193c612405a40eefe244ba59b599b
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 18:00:33 +0800

    Update __init__.py

--- criterions/binary_cross_entropy/__init__.py
+++ criterions/binary_cross_entropy/__init__.py
@@ -2,4 +2,11 @@
 """
 File __init__.py.py
 @author: ZhengYuwei
+多标签二分类损失函数及其变体 
 """
+from .basic_bce import MultiLabelBCELoss
+from .ghm_bce import GHMBCELoss
+from .hybrid_bce import HybridBCELoss
+from .ohm_bce import OHMBCELoss
+from .threshold_bce import ThresholdBCELoss
+from .weighted_bce import WeightedBCELoss
