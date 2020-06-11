a5a11034636e8320643d56a952190a991dee7100
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 17:03:56 +0800

    Update heatmap.py

--- utils/cam_tool/heatmap.py
+++ utils/cam_tool/heatmap.py
@@ -2,4 +2,26 @@
 """
 File heatmap.py
 @author: ZhengYuwei
+绘制热力图 
 """
+import cv2
+import numpy as np
+
+
+class HeatMapTool:
+
+    @staticmethod
+    def add_heat(image: np.ndarray, cams: np.ndarray) -> np.ndarray:
+        """ 给图像加heatmap
+        :param image: 单张图像，bgr uint8格式
+        :param cams: 类激活图，灰度图，uint8格式
+        :return: 原图 和 叠加了热力图图像 按宽方向拼接起来的图
+        """
+        h, w, nc = image.shape
+        cam_images = [image]
+        for cam in cams:
+            heatmap = cv2.applyColorMap(cam, cv2.COLORMAP_JET)
+            heatmap = cv2.resize(heatmap, (w, h))
+            cam_image = heatmap * 0.5 + image * 0.5
+            cam_images.append(cam_image)
+        return np.concatenate(cam_images, axis=1).astype(np.uint8)
