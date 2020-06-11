fa38bc485cc2d2946f9d43491cbeacab8dab49c1
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2020-06-11 19:56:24 +0800

    Update factory.py

--- models/efficientnet/factory.py
+++ models/efficientnet/factory.py
@@ -4,7 +4,7 @@ File factory.py
 @author: ZhengYuwei
 EfficientNet系列的工厂脚本 
 """
-from .model import EfficientNet
+from .efficientnet import EfficientNet
 
 __all__ = ['efficientnet_b0', 'efficientnet_b1', 'efficientnet_b2', 'efficientnet_b3',
            'efficientnet_b4', 'efficientnet_b5', 'efficientnet_b6', 'efficientnet_b7',
